bb62d47e6a4f833be2d795f984b54ca6ba90c46b
StQiLi <stevenqingli@gmail.com>
2024-04-01 21:52:14 -0400

    Committed some changes

--- website backend/test.js
+++ website backend/test.js
@@ -6,16 +6,19 @@ const vertexAI = new VertexAI({ project: 'iron-crane-418815', location: 'us-cent
 // Instantiate the model
 const generativeModel = vertexAI.getGenerativeModel({ model: 'gemini-1.0-pro' });
 
-async function createStreamChat() {
+async function createStreamChat(chatInput1) {
   const chat = generativeModel.startChat({});
-  const chatInput1 = 'How can I learn more about that?';
+  //const chatInput1 = 'How can I learn more about that?';
 
-  console.log(`User: ${chatInput1}`);
+  //console.log(`User: ${chatInput1}`);
 
   const result1 = await chat.sendMessageStream(chatInput1);
+  let str = "";
   for await (const item of result1.stream) {
     console.log(item.candidates[0].content.parts[0].text);
+    str = str + (item.candidates[0].content.parts[0].text);
   }
+  return str;
 }
 
 // Call the function when needed
